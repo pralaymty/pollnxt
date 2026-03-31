637ea068bc44829a2eabf09b5dcb8fdb18efe59c
pralaymty <pralay.mty@gmail.com>
2026-03-31 14:05:19 +0530

    Adding Favicon

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($page_title) ? h($page_title) . ' | ' : ''; ?>POLLNXT</title>
-    <link rel="icon" href="https://tentideconsultingservices.com/pollnxt/beta-v3/images/logo.png" type="image/png">
+    <link rel="icon" href="https://tentideconsultingservices.com/pollnxt/beta-v3/images/favicon.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
     <?php
